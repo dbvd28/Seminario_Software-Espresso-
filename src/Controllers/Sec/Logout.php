1f2065810ce3003eb3a004e030ec97ace2f42dfe
montoDennys <montoyadennys15@gmail.com>
2025-12-11 12:21:24 -0600

    Cambio css inicio y checkout

--- src/Controllers/Sec/Logout.php
+++ src/Controllers/Sec/Logout.php
@@ -2,19 +2,11 @@
 namespace Controllers\Sec;
 class Logout extends \Controllers\PublicController
 {
-<<<<<<< HEAD
     public function run(): void
-=======
-    public function run():void
->>>>>>> 4b948e4e086e285ce434b30daa28f700dffb1d71
     {
         \Utilities\Security::logout();
         \Utilities\Site::redirectTo("index.php");
     }
 }
 
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> 4b948e4e086e285ce434b30daa28f700dffb1d71
